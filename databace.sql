a6a0e4f09e40ce89a13b38362713a80c1f753275
AmalSamy <baamyaalan55@gmail.com>
2022-07-02 03:58:42 +0300

    DB file

--- databace.sql
+++ databace.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 29 يونيو 2022 الساعة 19:59
+-- Generation Time: 02 يوليو 2022 الساعة 02:57
 -- إصدار الخادم: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -35,6 +35,19 @@ CREATE TABLE `post_comment` (
   `created_datetime` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- إرجاع أو استيراد بيانات الجدول `post_comment`
+--
+
+INSERT INTO `post_comment` (`comment_id`, `post_id`, `userId`, `comment_text`, `created_datetime`) VALUES
+(1, 4, 4, 'a', '22:06:30 01:58:38 am'),
+(2, 4, 4, 'a', '22:06:30 02:00:11 am'),
+(3, 5, 5, 'اهلا', '22:06:30 11:32:14 am'),
+(4, 6, 7, 'amal', '22:06:30 12:01:00 pm'),
+(5, 6, 7, 'aml', ''),
+(6, 6, 7, 'aml', '22:06:30 11:22:03 am'),
+(7, 6, 7, 'aml', '22:06:30 12:24:13 pm');
+
 -- --------------------------------------------------------
 
 --
@@ -49,6 +62,14 @@ CREATE TABLE `users` (
   `name_img` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- إرجاع أو استيراد بيانات الجدول `users`
+--
+
+INSERT INTO `users` (`userId`, `username`, `email`, `password`, `name_img`) VALUES
+(7, 'amal', 'a@gmail.com', 'amal', 'i.png'),
+(8, 'amalsamy', 'amal@gmail.com', 'amal', 'maxresdefault.jpg');
+
 -- --------------------------------------------------------
 
 --
@@ -61,8 +82,21 @@ CREATE TABLE `user_post` (
   `written_text` varchar(255) NOT NULL,
   `comments` int(255) NOT NULL,
   `likes` int(255) NOT NULL,
-  `created_datetime` text NOT NULL
+  `created_datetime` text NOT NULL,
+  `img_url` int(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- إرجاع أو استيراد بيانات الجدول `user_post`
+--
+
+INSERT INTO `user_post` (`post_id`, `userId`, `written_text`, `comments`, `likes`, `created_datetime`, `img_url`) VALUES
+(1, 1, 'amal\r\n', 0, 0, '22:06:29 09:34:39 pm', NULL),
+(2, 1, 'a\r\n', 0, 0, '22:06:29 09:43:36 pm', NULL),
+(3, 1, 'a\r\n', 0, 0, '22:06:29 09:44:54 pm', NULL),
+(4, 4, 'amal', 0, 0, '22:06:30 01:58:04 am', NULL),
+(5, 5, 'مرحبا', 0, 0, '22:06:30 11:32:08 am', NULL),
+(6, 7, 'amal', 0, 0, '22:06:30 11:59:34 am', NULL);
 
 --
 -- Indexes for dumped tables
@@ -97,19 +131,19 @@ ALTER TABLE `user_post`
 -- AUTO_INCREMENT for table `post_comment`
 --
 ALTER TABLE `post_comment`
-  MODIFY `comment_id` int(255) NOT NULL AUTO_INCREMENT;
+  MODIFY `comment_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `userId` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `userId` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `user_post`
 --
 ALTER TABLE `user_post`
-  MODIFY `post_id` int(255) NOT NULL AUTO_INCREMENT;
+  MODIFY `post_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
